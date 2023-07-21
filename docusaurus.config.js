9512e8e09c2a65819db93175abd5355707965a41
Yuan.Wang7 <yuan.wang7@caocaoglobal.com>
2023-07-21 10:28:39 +0800

    feat(global): commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,9 +27,6 @@ const config = {
   // 静态资源文件夹
   // staticDirectories: ['public', 'static'],
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   // i18n: {
   //   defaultLocale: 'zh-Hans',
   //   locales: ['zh-Hans'],
